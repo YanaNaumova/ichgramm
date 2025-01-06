af67be44f1acabea304f2f89a3f3a93f3f603013
YanaNaumova <naumovayana87@gmail.com>
2025-01-06 12:22:20 +0100

    Project Structure and Server Initialization

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "../server/src/config/db.js";
+import authRoutes from "../server/src/routes/authRoutes.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+async function startServer() {
+  try {
+    await connectDB();
+
+    app.get("/", (req, res) => {
+      res.send("Hello world");
+    });
+
+    app.use("/api/auth", authRoutes);
+    app.listen(port, () => {
+      console.log(`Server running on ${port}`);
+    });
+  } catch (error) {
+    console.error("Failed connect to DB");
+    process.exit(1);
+  }
+}
+
+startServer();

9149a16f6c78c16cdf1f5cf59cf762ae7f8505ba
YanaNaumova <naumovayana87@gmail.com>
2025-01-22 19:36:08 +0100

    add docker

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
+import "dotenv/config";
 
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
+      // user: process.env.MONGO_INITDB_ROOT_USERNAME || "root",
+      // password: process.env.MONGO_INITDB_ROOT_PASSWORD || "root",
+      // user: process.env.MONGO_INITDB_ROOT_USERNAME || "root",
+      // password: process.env.MONGO_INITDB_ROOT_PASSWORD || "root",
+      authSource: "admin",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -10,6 +16,7 @@ async function connectDB() {
     console.log("Connect to DB");
   } catch (error) {
     console.error("Failed connect to DB");
+    console.error(error);
     process.exit(1);
   }
 }
---
+++ server/src/server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import likeRoutes from "./routes/likeRoutes.js";
+import commentRoutes from "./routes/commentRoutes.js";
+import searchRoutes from "./routes/searchRoutes.js";
+import exploreRoutes from "./routes/exploreRoutes.js";
+import followerRoutes from "./routes/followRoutes.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+const port = process.env.PORT || 3000;
+
+async function startServer() {
+  try {
+    await connectDB();
+
+    app.post("/api/hello", (req, res) => {
+      res.send("Hello world");
+    });
+
+    app.use("/api/auth", authRoutes);
+    app.use("/api/users", userRoutes);
+    app.use("/api/posts", postRoutes);
+    app.use("/api/likes", likeRoutes);
+    app.use("/api/comments", commentRoutes);
+    app.use("/api/search", searchRoutes);
+    app.use("/api/random", exploreRoutes);
+    app.use("/api/follower", followerRoutes);
+    app.listen(port, () => {
+      console.log(`Server running on ${port}`);
+    });
+  } catch (error) {
+    console.error("Failed connect to DB");
+    process.exit(1);
+  }
+}
+
+startServer();

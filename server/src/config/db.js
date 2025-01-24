c7949f158fe9697e1c1263ec8d559e05d5d13f49
YanaNaumova <naumovayana87@gmail.com>
2025-01-24 12:35:27 +0100

    docker

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,11 +3,14 @@ import "dotenv/config";
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI_LOCAL || MONGO_URI_ATLAS, {
-      authSource: "admin",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      process.env.MONGO_URI_ATLAS || process.env.MONGO_URI_LOCAL,
+      {
+        authSource: "admin",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
     console.log("Connect to DB");
   } catch (error) {

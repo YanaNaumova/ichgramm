3219087399dd6ba6817f373d4071fc7391be91b6
YanaNaumova <naumovayana87@gmail.com>
2025-04-01 02:30:18 +0200

    change localhost

--- client/src/api/authApliClient.js
+++ client/src/api/authApliClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authApiClient = axios.create({
-  baseURL: "https://ichgramm.onrender.com/api",
+  baseURL: "http://localhost:3000/api",
 });
 
 export default authApiClient;
